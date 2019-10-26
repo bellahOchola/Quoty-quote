7b4ba694154f5f5e0d23c37522cb5b472e05dc04
bellahOchola <bellahkenya@gmail.com>
2019-10-26 06:03:38 +0300

    add an array in the quotes ts file

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,5 @@
 export class Quotes {
-    constructor(
-        public description:string,
-        public author: string,
+    constructor(public description:string,
+        public author: string
     )
 }
