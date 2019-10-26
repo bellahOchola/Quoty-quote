f089dec0ea9ede4b595ccf7aa98c4adbbc61ed6e
bellahOchola <bellahkenya@gmail.com>
2019-10-26 12:53:25 +0300

    change background image

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,10 +9,10 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes: Quotes[] = [
-    new Quotes('Change your thoughts and you change your  word', 'By Norman Vincent', new Date(2002,5,12)),
-    new Quotes('Act as if what you do makes a difference to others', 'By William James', new Date(2009,3,8)),
-    new Quotes('When you cannot find the sunshine Be the sunshine', 'By Mash James', new Date(2012,9,16)),
-    new Quotes('Never allow someone to be your priority while you are their slave', 'By Catherine Carter', new Date(2015,7.28))
+    new Quotes('Change your thoughts and you change your  word', ' Norman Vincent', new Date(2002,5,12)),
+    new Quotes('Act as if what you do makes a difference to others', 'William James', new Date(2009,3,8)),
+    new Quotes('When you cannot find the sunshine Be the sunshine', 'Mash James', new Date(2012,9,16)),
+    // new Quotes('Never allow someone to be your priority while you are their slave', ' Catherine Carter', new Date(2015,7.28))
   ];
   constructor() { }
 
