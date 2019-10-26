38709ca85edbaed6aa81cd5d8ffadf5a9cce2cf3
bellahOchola <bellahkenya@gmail.com>
2019-10-26 06:01:11 +0300

    add constructor function to the class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    constructor(
+        public description:string,
+        public author: string,
+    )
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes: Quotes[] = [
+    new Quotes('Enjoy the Little Things', 'By James Adam')
+  ];
   constructor() { }
 
   ngOnInit() {
