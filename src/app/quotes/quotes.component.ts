04d516895b0acf2734212a89901e3d949c04fb24
bellahOchola <bellahkenya@gmail.com>
2019-10-27 19:48:27 +0300

    change background image and color

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,7 +9,7 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes: Quotes[] = [
-    new Quotes('Change your thoughts and you change your  word', ' Norman Vincent', 'vic', new Date(2002,5,12),0,0),
+    new Quotes('Two things are infinite: the universe and human stupidity and I am not sure about the universe', ' Norman Vincent', 'vic', new Date(2002,5,12),0,0),
     new Quotes('Act as if what you do makes a difference to others', 'William James', 'melvin', new Date(2009,3,8),0,0),
     new Quotes('When you cannot find the sunshine Be the sunshine', 'Mash James', 'carly', new Date(2012,9,16),0,0),
     // new Quotes('Never allow someone to be your priority while you are their slave', ' Catherine Carter', new Date(2015,7.28))
